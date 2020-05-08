4be2019beab8d5432e1bfbc0d50a86a8f38c675d
Your Name <you@example.com>
2020-05-08 11:12:55 -0400

    Added some demo stuff

--- demo.sh
+++ demo.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
 
+DIR=/tmp/clutter
+NUMTRAIN=70
+NUMVAL=1
+NUMTOT=$((NUMTRAIN+NUMVAL))
+rm -rf $DIR
+
 echo "This is the demo of the CV2 code"
 
 echo "We will start walking through all of the major components"
 
-echo "Now generating 100 synthetic images, and saving them to /tmp/clutter/train"
+echo "Now generating $NUMTOT synthetic images, and saving them to $DIR/train"
+./clutterizer/clutterize.py -s $DIR/train/generated.jpg -n $NUMTOT
+./solution_final/labelme2coco.py $DIR/train $DIR/train
 
-./clutterizer/clutterize.py -s /tmp/clutter/train/generated.jpg -n 100
-./solution_final
+mkdir $DIR/validate
+for num in `seq $NUMTRAIN $((NUMTOT-1))`; do
+    mv $DIR/train/generated$num.* $DIR/validate
+done
+cp $DIR/train/names.txt $DIR/validate
+
+echo "Now calculating bounding boxes for $NUMVAL validation images"
+./solution_final/solution.py --parallel False $DIR/train $DIR/validate $DIR/answers
+./solution_final/evaluate.py $DIR/answers $DIR/validate
+
+echo "Now displaying the images. Press any key to continue."
+./solution_final/visualize.py $DIR/answers $DIR/validate
+
+mkdir $DIR/test
+clutterDir=`grep 'clutter:' conf.yaml | awk '{print $2}'`
+for file in `find $clutterDir | grep json`; do
+    jpg="`cut -d'.' -f1 <<< $file`.jpg"
+    cp $file $DIR/test
+    cp $jpg $DIR/test
+    break
+done
+cp $DIR/train/names.txt $DIR/test
+./labelme2coco.py $DIR/test $DIR/test
+
+echo "Now calculating bounding boxes for 1 test image"
+./solution.py --parallel False $DIR/train $DIR/test $DIR/testAnswers
+./evaluate.py $DIR/testAnswers $DIR/test
+
+echo "Now displaying the images. Press any key to continue."
+./solution_final/visualize.py $DIR/testAnswers $DIR/test
--- solution_final/histogram.py
+++ solution_final/histogram.py
@@ -9,27 +9,15 @@ from sklearn import neighbors
 import scipy
 
 model = None
-#modelOneClass = None
 modelOneClasses = {}
 histograms = None
 
-# Augment: texture background, lighting, saturation
-
 def registerGroundTruth(truth):
     global model
-    #global modelOneClass
     global modelOneClasses
     global histograms
     global kernel
-    #model = svm.LinearSVC(max_iter=10000)
     model = svm.NuSVC(kernel=kernel)
-    #model = svm.NuSVC(kernel='sigmoid')
-    #modelOneClass = svm.OneClassSVM(gamma='auto', kernel='sigmoid')
-    #modelOneClass = svm.OneClassSVM(kernel='sigmoid')
-    #modelOneClass = svm.OneClassSVM()
-    #modelOneClass = svm.OneClassSVM(gamma='auto', kernel='poly')
-    #modelOneClass = ensemble.IsolationForest()
-    #modelOneClass = neighbors.LocalOutlierFactor(novelty=True)
     for obj in truth:
         modelOneClasses[obj] = neighbors.LocalOutlierFactor(novelty=True)
     histograms = truth
@@ -44,19 +32,13 @@ def registerGroundTruth(truth):
         modelOneClasses[obj].fit(objData)
     print('Fitting model to data')
     model.fit(data, labels)
-    #modelOneClass.fit(data)
 
 def getObject(image, mask=None):
     global model
     global modelOneClasses
-    #global modelOneClass
     global histograms
     hist = getHistogramData(image, mask)
-    #obj = model.predict(hist.reshape(1, -1))[0]
-    #conf = max(compareHistogramData(hist, hist2) for hist2 in histograms[obj])
-    #if modelOneClass.predict(hist.reshape(1, -1))[0] == -1:
     obj = model.predict(hist)[0]
-    #conf = max(compareHistogramData(hist, hist2) for hist2 in histograms[obj])
     conf = 0
     for histogram in histograms[obj]:
         score = compareHistogramData(hist, histogram)
@@ -64,20 +46,11 @@ def getObject(image, mask=None):
             conf = score
     
     score = modelOneClasses[obj].score_samples(hist)[0]
-    #score = modelOneClass.score_samples(hist)[0]
-    print('One class score: {}'.format(score))
-    print('One class probability: {}'.format(np.exp(score)))
-    #if modelOneClass.predict(hist)[0] == -1:
-    #    conf /= 2
     conf *= np.exp(score)
-    #cv2.imshow('Object', cv2.bitwise_and(image, image, mask=mask))
-    #cv2.imshow('Match', img)
-    #cv2.waitKey()
     return obj, conf
 
 def getHistogramData(image, mask=None):
     getters = [getHistogramDataLBP, getHistogramDataColor, getVariance]
-    #getters = [getHistogramDataLBP, getHistogramDataColor]
     data = np.array([])
     for getter in getters:
         data = np.append(data, getter(image, mask))
@@ -88,8 +61,6 @@ def getHistogramDataColor(image, mask=None):
     # We only care about channel 0 (h)
     image = image[:,:,0]
     hist = cv2.calcHist([image], [0], mask, [32], [0,256])
-    #hist = cv2.calcHist([image], [0], mask, [256], [0,256])
-    #hist = np.array([cv2.calcHist([image], [channel], mask, [256], [0,256]) for channel in range(image.shape[2])])
     hist = hist.reshape(1, -1)
     hist = hist.astype('float')
     hist /= (hist.sum() + 1e-1)
@@ -99,13 +70,10 @@ def getHistogramDataLBP(image, mask=None):
     global lbpIndex
     # params = [ (numPoints, radius), ...]
     params = [(24, 8), (16, 4), (12, 2), (8, 1)]
-    #params = [(24, 8), (16, 4), (8, 1)]
     params = [params[i] for i in range(len(params)) if i != lbpIndex]
     if mask is not None:
         image = cv2.bitwise_and(image, image, mask=mask)
-    #print('About to cvtColor')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #print('About to calc lbp hist')
     hists = []
     for numPoints, radius in params:
         lbp = feature.local_binary_pattern(image, numPoints, radius, method='uniform')
